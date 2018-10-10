c0b3a96a12abc447106151ac358d595f17734fef
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-10 17:25:25 +0300

    AJAX content

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 import datetime as dt
 
 class Editor(models.Model):
@@ -37,10 +39,10 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length= 60)
     post = models.TextField() 
-    editor = models.ForeignKey(Editor)       
+    editor = models.ForeignKey(User, on_delete=models.CASCADE)       
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/')
+    article_image = models.ImageField(upload_to = 'articles/', blank=True)
 
     def save_article(self):
         self.save()
@@ -62,4 +64,10 @@ class Article(models.Model):
     @classmethod
     def search_by_title(cls,search_term):
         news = cls.objects.filter(title__icontains=search_term)
-        return news    
+        return news 
+    def __str__(self):
+        return self.title    
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
--- tribune/news/views.py
+++ tribune/news/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
+from .forms import NewArticleForm, NewsLetterForm
+from .email import send_welcome_email
 
 # Create your views here.
 def welcome(request):
@@ -10,8 +13,9 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    form = NewsLetterForm()
+    
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 #view function to present news from past days
 def past_days_news(request, past_date):
@@ -46,6 +50,7 @@ def search_results(request):
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html', {"message":message})    
 
+@login_required(login_url='/accounts/login/')
 def article(request, article_id):
     
     try:
@@ -54,4 +59,29 @@ def article(request, article_id):
     except DoesNotExist:
         raise Http404()
     
-    return render(request, "all-news/article.html", {"article":article})            
+    return render(request, "all-news/article.html", {"article":article})   
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
